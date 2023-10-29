e5177af155e3dfaae2c22f8e93de159e5b41e9b4
BinHawkins42 <36099433+BinHawkins42@users.noreply.github.com>
2023-10-29 00:10:00 -0500

    added discovery page and updates to home chamber

---
+++ chamber/scripts/dates-visited.js
@@ -0,0 +1,20 @@
+const today = Date.now();
+
+localStorage.setItem('dateVisited', today);
+
+const lastVisit = localStorage.getItem('dateVisited');
+
+const numberOfDays = lastVisit + Date.now();
+
+if (numberOfDays = 1) {
+    let output = "Welcome! Let us know if you have any questions";
+}
+else if (numberOfDays = 2) {
+    let output = "Back so soon! Awesome!";
+}
+
+else if (numberOfDays < 2 ) {
+    let output = "welcome back it has been" + numberOfDays;
+ }
+
+ document.getElementById("output").innerHTML = output;
