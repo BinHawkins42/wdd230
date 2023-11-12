226fbd63d7a5c056517b555cf68dec9abb134133
BinHawkins42 <36099433+BinHawkins42@users.noreply.github.com>
2023-11-11 22:38:55 -0600

    fixed links

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -33,7 +33,7 @@ const displayMembers = (members) => {
       phone.textContent = `${members.phonenumber}`;
 
       website.setAttribute('href', members.url );
-      website.textContent = `${member.url}`;
+      website.textContent = `${members.url}`;
 
 
   
