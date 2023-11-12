f63a00dff6210639d41a58360a8f52c698b4b5ab
BinHawkins42 <36099433+BinHawkins42@users.noreply.github.com>
2023-11-11 23:41:19 -0600

    changed images and updated CSS

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -33,7 +33,7 @@ const displayMembers = (members) => {
       phone.textContent = `${members.phonenumber}`;
 
       website.setAttribute('href', members.url );
-      website.textContent = `${members.url}`;
+      website.textContent = `${members.name} website`;
 
 
   
@@ -47,3 +47,20 @@ const displayMembers = (members) => {
       businesses.appendChild(card);
     });
   }
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); 
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
