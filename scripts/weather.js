dec69db98c4e97999ab44bd0911b06313827bbf8
BinHawkins42 <36099433+BinHawkins42@users.noreply.github.com>
2023-11-16 23:23:29 -0600

    added weather to main page

---
+++ scripts/weather.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=36.70&lon=-93.21&appid=ffbdc82e21c26c6f1453aa3a21f77a5a&units=imperial';
+
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+    } else {
+        throw Error (await response.text());
+    }
+} catch (error) {
+    console.log(error);
+}
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('img', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+ 
+}
+
+505819431765, 870028902117
