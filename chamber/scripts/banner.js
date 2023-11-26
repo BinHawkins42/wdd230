cd5c272b06be21221e07ae9c2a3da84e8b8f9038
BinHawkins42 <36099433+BinHawkins42@users.noreply.github.com>
2023-11-25 22:05:09 -0600

    fixed json

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,17 +1,53 @@
-document.querySelector('.banner__close').addEventListener('click', function() {
-        this.closest('.banner').style.display = 'none';
+document.addEventListener('DOMContentLoaded', () => {
+
+function isBannerDay() {
+     const today = new Date().getDay(); // [0 = Sunday, 1 = Monday, 2 = Tuesday, Wenesday = 3]
+     return today >= 1 && today <= 3; // Monday, Tuesday, or Wednesday
+ }
+
+ function showBanner() {
+     const banner = document.getElementById('banner');
+     if (isBannerDay()) {
+         banner.style.display = 'block';
+     }
+     else{
+          banner.style.display = 'none';
+     }
+ }
+
+ // Function to hide the banner
+ function hideBanner() {
+     const banner = document.getElementById('banner');
+     banner.style.display = 'none';
+ }
+
+ function closeBanner() {
+     hideBanner();
+ }
+
+ showBanner()
+
+ const closeButton = document.getElementById('close__banner');
+ if (closeButton) {
+     closeButton.addEventListener('click', closeBanner);
+ }
+
+//  document.querySelector('.banner__close').addEventListener('click', function() {
+//      this.closest('.banner').style.display = 'none';
+// });
+
 });
 
-function myDay() {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const date = new Date();
-    const banner = document.getElementsByClassName('.banner')
+// function myDay() {
+//     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     const date = new Date();
+//     const banner = document.getElementsByClassName('.banner')
 
-    if(date == days(0,1,2,3)){
-        banner = style.display = 'block';
+//     if(date == days(0,1,2,3)){
+//         banner = style.display = 'block';
          
 
-   }else{
-        banner = style.display = 'none'; 
-   } 
-  }
+//    }else{
+//         banner = style.display = 'none'; 
+//    } 
+//   }
--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,19 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
 const baseURL = "https://binhawkins42.github.io/wdd230/";
-const url = "https://binhawkins42.github.io/wdd230/chamber/data/members.json";
-const businesses = document.querySelector('.spotlight');
+const membersUrl = "https://binhawkins42.github.io/wdd230/chamber/data/members.json";
+const spotlightContainer = document.getElementById("spotlight");
 
-async function getMembersData() {
-    const responce = await fetch(url);
-    const data = await responce.json();
-    displayMembers(data.members);
+async function getMembersData(Url) {
+    try {
+        const response = await fetch(Url);
+        const data = await response.json();
+        displaySpotlightMembers(data.members);
+    } catch (error) {
+        console.error('Error fetching or processing data:', error);
+    }
 }
 
-getMembersData();
+function getRandomMembers(data, count) {
+    const shuffledMembers = data.sort(() => 0.5 - Math.random());
+    return shuffledMembers.slice(0, count);
+}
 
-const displayMembers = (members) => {
-    members.forEach((members) => {
+function displaySpotlightMembers(members) {
+    const spotlightMembers = getRandomMembers(members, 3);
 
-      let card = document.createElement('section');
+    spotlightMembers.forEach(members => {
+        const card = createSpotlightCard(members);
+        spotlightContainer.appendChild(card);
+    });
+}
+
+function createSpotlightCard(members) {
+   
+
+      let card = document.createElement('div');
+      card.classList.add('spotlight');
+
       let fullName = document.createElement('h3'); 
       let address = document.createElement('p');
       let phone = document.createElement('p');
@@ -36,15 +55,95 @@ const displayMembers = (members) => {
       website.setAttribute('href', members.url );
       website.textContent = `${members.name} website`;
 
-      level.textContent = `${member.level}`;
+      level.textContent = `${members.level}`;
   
       card.appendChild(logo);
       card.appendChild(fullName); 
       card.appendChild(address);
       card.appendChild(phone);
       card.appendChild(website);
+      card.appendChild(level);
 
   
-      businesses.appendChild(card);
-    });
+      return card;
+ 
   }
+
+getMembersData(membersUrl);
+});
+// const businesses = document.querySelector('.cards');
+// const randomIndex = document.querySelector('.spotlight');
+
+
+
+
+// // const getRandomMembers = () => {
+// //     const randomMembers = [];
+// //     while (randomMembers.length < 3) {
+// //       const randomMember = getRandomMember();
+// //       // Avoid duplicates
+// //       if (!randomMembers.some( member => member === randomMember)) {
+// //         randomMembers.push(randomMember);
+// //       }
+// //     }
+// //     return randomMembers;
+// //   };
+
+// async function getMembersData() {
+//     const responce = await fetch(urlSpot);
+//     const data = await responce.json();
+//     displayMembers(data.members);
+// }
+
+// const getRandomMember = () => {
+//     const randomIndex = Math.floor(Math.random() * businesses);
+//     return  randomIndex;
+//   };
+
+// const displayMembers = (members) => {
+//     members.forEach((members) => {
+
+//       let card = document.createElement('section');
+//       let fullName = document.createElement('h3'); 
+//       let address = document.createElement('p');
+//       let phone = document.createElement('p');
+//       let website = document.createElement('a')
+//       let logo = document.createElement('img');
+//       let level = document.createElement('h3')
+  
+//       logo.setAttribute('src', members.imageurl);
+//       logo.setAttribute('alt', `logo for ${members.name}`); 
+//       logo.setAttribute('loading', 'lazy');
+//       logo.setAttribute('width', '150');
+//       logo.setAttribute('height', 'auto');
+      
+//       fullName.textContent = `${members.name}`; 
+
+//       address.textContent = `${members.address}`;
+
+//       phone.textContent = `${members.phonenumber}`;
+
+//       website.setAttribute('href', members.url );
+//       website.textContent = `${members.name} website`;
+
+//       level.textContent = `${members.level}`;
+  
+//       card.appendChild(logo);
+//       card.appendChild(fullName); 
+//       card.appendChild(address);
+//       card.appendChild(phone);
+//       card.appendChild(website);
+//       card.appendChild(level);
+
+  
+//       businesses.appendChild(card);
+//     });
+//   }
+
+//   getMembersData();
+
+
+
+
+
+ 
